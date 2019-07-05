2a2daec00e95af529be1cd357560b9a442db0b93
nthhanh <nthhanh111994@gmail.com>
2019-07-05 11:30:28 +0700

    Change name Hanh -> Hanh Nguyen

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ name: 'Hanh' });
+	res.send({ name: 'Hanh Nguyen' });
 });
 
 const PORT = process.env.PORT || 8000;
